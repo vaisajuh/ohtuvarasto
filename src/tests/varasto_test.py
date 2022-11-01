49cdc289d7644bec3a53d62fa131eaff8ecba845
vaisajuh <juha.vaisanen@gmail.com>
2022-11-01 19:53:00 +0200

    varasto

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -12,11 +12,25 @@ class TestVarasto(unittest.TestCase):
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+    
+    def test_uudella_varastolla_vaara_tilavuus_ja_saldo(self):
+        self.varasto = Varasto(-1,-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_saldo_pienempi_ja_isompi_kuin_tilavuus(self):
+        self.varasto1 = Varasto(1,3)
+        self.varasto2 = Varasto(3,1)
+        self.assertAlmostEqual(self.varasto1.tilavuus, 1)
+        self.assertAlmostEqual(self.varasto2.tilavuus, 3)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.assertAlmostEqual(self.varasto.saldo, 8)
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -26,10 +40,12 @@ class TestVarasto(unittest.TestCase):
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
-
         saatu_maara = self.varasto.ota_varastosta(2)
-
         self.assertAlmostEqual(saatu_maara, 2)
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0)
+        saatu_maara = self.varasto.ota_varastosta(100)
+        self.assertAlmostEqual(saatu_maara, 6)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +54,7 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_string(self):
+        joo = self.varasto.__str__()
+        self.assertEqual(joo, "saldo = 0, vielä tilaa 10")
